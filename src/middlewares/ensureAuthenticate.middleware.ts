d6622bb70ed263ed3276d6f4434656f9429c6282
Artur Watanabe <arturwatanabe@gmail.com>
2023-05-25 04:50:54 -0300

    add: authenticate middleware

---
+++ src/middlewares/ensureAuthenticate.middleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { JWTToken } from "../utils/token/Implementations/token.jsonwebtoken";
+
+export const ensureAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const headerAuth = req.headers.authorization;
+
+  if (!headerAuth) {
+    return res.status(401).json({
+      message: "Ops, paprece que voce ainda nao efetuou o login",
+    });
+  }
+  const [, token] = headerAuth.split(" ");
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Ops, paprece que voce ainda nao efetuou o login",
+    });
+  }
+  const verifyIfTokenIsValid = new JWTToken().validate(token);
+  if (verifyIfTokenIsValid) {
+    return next();
+  }
+  return res.status(401).json({ message: "Seu token de autorizacao expirou" });
+};
