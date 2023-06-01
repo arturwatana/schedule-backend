263b973086a470a2d7be3c8484be154d66b89894
Artur Watanabe <arturwatanabe@gmail.com>
2023-06-01 06:08:05 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,14 @@ AppDataSource.initialize()
   .then(async () => {
     app.use(express.json());
     app.use(cors());
+    app.use((req, res, next) => {
+      res.set(
+        "Access-Control-Allow-Origin",
+        "https://schedule-react-nu.vercel.app"
+      );
+      res.set("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+    });
     app.use(userRoutes);
     app.use(taskRoutes);
     app.listen(8080, () => {
